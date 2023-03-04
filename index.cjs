4f464011a52061c494df550944c4a1f4cdc5567a
Soltus <liaoshanyi@qq.com>
2023-03-05 00:11:07 +0800

    6

---
+++ index.cjs
@@ -0,0 +1,9 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Sofill = void 0;
+const api_1 = require("./api");
+class Sofill {
+    API = api_1.API;
+}
+exports.Sofill = Sofill;
